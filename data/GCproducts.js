f40bcbc21ff9a66fea210c77fefcda225767baed
trevezesyx-cyber <trevezesyx@gmail.com>
2026-04-14 01:11:23 +0800

    details change

--- data/GCproducts.js
+++ data/GCproducts.js
@@ -4,24 +4,24 @@ const PRODUCTS = [
     partname: "GC Collection™",
     description: "Our own range of ready-made repeating patterns.",
     designType: "Patterns",
-    image: ""
+    image: "../assets/E-cat/Product/GC/GCC.png"
   },
   {
     partname: "Custom-made",
     description: "Stop searching and start creating: Your custom masterpiece is only moments away.",
     designType: "Patterns",
-    image: ""
+    image: "../assets/E-cat/Product/GC/custom made.png"
   },
   {
     partname: "GC Smooth",
     description: "A blank membrane to create a beautiful velvety smooth and even concrete surface without any pattern.",
     designType: "Smooth",
-    image: ""
+    image: "../assets/E-cat/Product/GC/smooth.png"
   },
   {
     partname: "GC Expose",
     description: "Helps provide a fully exposed surface in a safe and environmentally friendly way.",
     designType: "Expose",
-    image: ""
+    image: "../assets/E-cat/Product/GC/exposed.png"
   }
 ];
